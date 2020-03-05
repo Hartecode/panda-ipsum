5cf2b1df6f87a49473866f2c42523ace2d9cb95e
Sean Harte <>
2020-03-05 15:41:24 -0500

    added ipsum api

---
+++ pages/api/ipsum/[num].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import IpsumGenerator from '../../../utils/ipsum-generator';
+import * as pandaData from '../../../utils/panda.json'
+const ipsumInstance = new IpsumGenerator(pandaData.original_text);
+
+
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { num } = req.query
+    const numOfPar = Number(num);
+    console.log(numOfPar)
+     
+    if (isNaN(numOfPar)) {
+      throw new Error('Must send a number')
+    }
+
+    if (numOfPar < 1) {
+      throw new Error('The request at least needs to be 1')
+    }
+
+    if (numOfPar > 99) {
+      throw new Error('The request must be 99 and under')
+    }
+
+    const richIpsumArray = ipsumInstance.generateIpsumArray(numOfPar);
+
+    res.status(200).json({richIpsumArray})
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ statusCode: 400, message: err.message })
+  }
+}

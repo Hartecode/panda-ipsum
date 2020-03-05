36e11b27db2dcb86aff648b0c799ac1437031aab
Sean Harte <>
2020-03-05 15:49:57 -0500

    added to home page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,54 @@
-import * as React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
+import fetch from 'isomorphic-unfetch'
+import IpsumSection from '../components/lpsumSection'
+import useSWR from 'swr'
+import IpsumUpdateForm from '../components/IpsumUpdateForm'
+
+interface IpsumResponse {
+  richIpsumArray: string[];
+}
+
+async function fetcher(url: string): Promise<Response> {
+  const r = await fetch(url)
+  return await r.json()
+}
 
 const IndexPage: NextPage = () => {
+  const [count, setCount] = useState(1);
+  const [ipsumArr, setIpsumArr] = useState();
+
+  const { data, error } = useSWR('/api/ipsum/1', fetcher);
+  let firstLoad: string[];
+  if (error) firstLoad = ['Sorry there was an issue']
+
+  firstLoad = (data as unknown as IpsumResponse)?.richIpsumArray || ['']
+
+
+  const updateIpsum = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const json = await fetcher(`./api/ipsum/${count}`)
+    setIpsumArr((json as unknown as IpsumResponse)?.richIpsumArray)
+  }
+
+  const onCopy = () => {
+    console.log('copy')
+  }
+
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
+    <Layout title="Panda Ipsum">
+      <h1>Hello, Panda 👋</h1>
+      <IpsumUpdateForm 
+        onSubmit={updateIpsum}
+        count={count}
+        onChange ={(e) => setCount(Number(e.target.value))}/>
+      <button onClick={onCopy}>
+        Copy
+      </button>
+      <IpsumSection
+        items={ipsumArr ? ipsumArr : firstLoad}>
+      </IpsumSection>
     </Layout>
   )
 }

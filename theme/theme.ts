0e778f100180b1bdf03b6d0c587be187b1299778
Sean Harte <>
2020-03-11 13:53:38 -0400

    added theme file

---
+++ theme/theme.ts
@@ -0,0 +1,29 @@
+export const buttonColor = '#0b5d0b'
+export const textColor1 = `#313131`
+export const textColor2 = '#fff'
+export const layoutWidth = '62.5rem'
+export const logoImgMin = '6rem'
+export const logoImgMax = '8rem'
+
+// background colors
+export const backgroundColor100 = '#073707'
+export const backgroundColor200 = '#118511'
+export const backgroundColor300 = '#654100'
+
+//spacing 
+export const spacing25 = '0.0625rem' //1px
+export const spacing50 = '0.125rem' // 2px
+export const spacing75 = '0.1875rem' // 3px
+export const spacing100 = '0.25rem' // 4px
+export const spacing200 = '0.375rem' // 6px
+export const spacing300 = '0.9375rem' //15px
+export const spacing400 = '1rem' //16px
+export const spacing500 = '1.5rem'
+export const spacing600 = '1.5625rem'  //25px
+export const spacing700 = '1.75rem' //28px
+export const spacing800 = '2rem'
+export const spacing900 = '3rem'
+export const spacing1000 = '4rem'
+export const spacing1100 = '5.5rem'
+export const spacing1200 = '8rem'
+export const spacing1300 =  '12rem'

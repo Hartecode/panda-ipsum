8f01f1de1b6e325fed89b452290cfc291827bb9c
Sean Harte <>
2020-03-12 09:28:08 -0400

    updated themes file

--- theme/theme.ts
+++ theme/theme.ts
@@ -9,6 +9,7 @@ export const logoImgMax = '8rem'
 export const backgroundColor100 = '#073707'
 export const backgroundColor200 = '#118511'
 export const backgroundColor300 = '#654100'
+export const backgroundColor400 = '#f3f3f3'
 
 //spacing 
 export const spacing25 = '0.0625rem' //1px
@@ -27,3 +28,15 @@ export const spacing1000 = '4rem'
 export const spacing1100 = '5.5rem'
 export const spacing1200 = '8rem'
 export const spacing1300 =  '12rem'
+
+// font size
+export const fontSize50 = '0.75rem'; // 12
+export const fontSize100 = '0.875rem'; // 14
+export const fontSize200 = '1rem'; // 16
+export const fontSize300 = '1.125rem'; // 18
+export const fontSize400 = '1.25rem'; // 20
+export const fontSize500 = '1.5rem'; // 24
+export const fontSize600 = '2rem'; // 32
+export const fontSize700 = '2.5rem'; // 40
+export const fontSize800 = '3rem'; // 48
+export const fontSize900 = '4rem'; // 64

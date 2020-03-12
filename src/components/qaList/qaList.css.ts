eeafda06dc40033990cdf9a8b7aeb7f6ea6042aa
Sean Harte <>
2020-03-12 11:59:55 -0400

    adjusted spacing

--- src/components/qaList/qaList.css.ts
+++ src/components/qaList/qaList.css.ts
@@ -5,8 +5,8 @@ import * as themes from '../../theme/theme'
 export default css `
   .root {
     display: grid;
-    grid-gap: ${themes.spacing200};
-    margin: 0;
+    grid-row-gap: ${themes.spacing400};
+    margin: ${themes.spacing800} 0;
   }
 
   dt {
@@ -17,4 +17,17 @@ export default css `
   dd {
     margin: 0;
   }
+
+  @media screen and (min-width: 43.75rem) {
+
+    .root {
+      grid-column-gap: ${themes.spacing500};
+    }
+
+    dd {
+      margin: 0;
+      grid-row: 2;
+    }
+  }
+
 `

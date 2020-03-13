32141efb3b06d0cc9346d4b61457c724ed175cfd
Sean Harte <>
2020-03-13 13:32:44 -0400

    added serve side rendering with data routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '../components/layout/Layout'
 import { NextPage } from 'next'
-import Landing from '../components/landing/landing'
+import Landing, { IpsumResponse } from '../components/landing/landing'
+import { env } from '../environment/environment'
 
-const IndexPage: NextPage = () =>  (
+async function fetcher<T>(url: string): Promise<T> {
+  const r = await fetch(url)
+  return await r.json()
+}
+
+const IndexPage: NextPage<IpsumResponse> = (props) =>  (
   <Layout title="Panda Ipsum">
-    <Landing></Landing>
+    <Landing {...props}></Landing>
   </Layout>
 )
+
+IndexPage.getInitialProps = async function() {
+  
+  const data = await fetcher<IpsumResponse>(`${env.hostUrl}api/ipsum/1`);
+
+  return {
+    ipsumArray: data.ipsumArray
+  };
+};
 
 export default IndexPage

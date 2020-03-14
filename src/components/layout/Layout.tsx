2d24601fba1fa8e10acd1ceb7107137d97b09c53
Sean Harte <scsharte@gmail.com>
2020-03-14 11:09:11 -0400

    added other image sizes

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -20,7 +20,7 @@ const Layout: React.FunctionComponent<Props> = ({
       <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
       <meta name="description" content="Panda ipsum is a text generator for placeholder text. Great for people who want their love of pandas to show in their development process."/>
       <meta name="keywords" content="Panda, Ipsum, panda, ispum, Panda Ipsum, panda ipsum, developer, programer, placeholder text, lorem ipsum, lorem"/>
-      <link rel="icon" href="./img/panda.png"/>
+      <link rel="icon" href="./img/panda-70x72.png"/>
       <link href="https://fonts.googleapis.com/css?family=Baloo+2:400,700&display=swap" rel="stylesheet"></link>
     </Head>
     <Header></Header>
